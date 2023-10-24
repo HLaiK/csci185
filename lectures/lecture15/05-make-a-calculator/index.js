e10d179ae8f3bd88ae6007a8297c8245aab6f3e9
HLaiK <lktul@unca.edu>
2023-10-23 20:05:48 -0400

    Adding my homepage

--- lectures/lecture15/05-make-a-calculator/index.js
+++ lectures/lecture15/05-make-a-calculator/index.js
@@ -2,6 +2,13 @@ function addNumbers() {
     // Your job: 
     // 1. Access the first number that the user typed in and store it
     //    as a variable.
+    let num1 = document.querySelector("#num1").value;
+    num1 = Number(num1);
+    let num2 = document.querySelector("#num2").value;
+    num2 = Number(num2);
+    let sum = num1 + num2;
+    console.log(sum);
+    document.querySelector("#answer").innerHTML = sum;
     // 2. Access the first number that the user typed in and store it
     //    as a variable.
     // 3. Convert the values stored inside of the two variables to a 
@@ -12,13 +19,34 @@ function addNumbers() {
 
 function subtractNumbers() {
     // Same as above but subtract!
+    let num1 = document.querySelector("#num1").value;
+    num1 = Number(num1);
+    let num2 = document.querySelector("#num2").value;
+    num2 = Number(num2);
+    let sum = num1 - num2;
+    console.log(sum);
+    document.querySelector("#answer").innerHTML = sum;
 }
 
 function multiplyNumbers() {
     // Same as above but multiply!
+    let num1 = document.querySelector("#num1").value;
+    num1 = Number(num1);
+    let num2 = document.querySelector("#num2").value;
+    num2 = Number(num2);
+    let sum = num1 * num2;
+    console.log(sum);
+    document.querySelector("#answer").innerHTML = sum;
 }
 
 function divideNumbers() {
     // Same as above but divide!
+    let num1 = document.querySelector("#num1").value;
+    num1 = Number(num1);
+    let num2 = document.querySelector("#num2").value;
+    num2 = Number(num2);
+    let sum = num1 / num2;
+    console.log(sum);
+    document.querySelector("#answer").innerHTML = sum;
 }
 
---
+++ lectures/lecture16/02-function-with-parameters/main.js
@@ -0,0 +1,16 @@
+// make a function:
+// function turnRed(){
+//     document.querySelector("body").style.backgroundColor = "red";
+// }
+// function turnBlue(){
+//     document.querySelector("body").style.backgroundColor = "Blue";
+// }
+// function turnGreen(){
+//     document.querySelector("body").style.backgroundColor = "Green";
+// }
+
+function changeBackground(color){
+    document.querySelector("body").style.backgroundColor = color;
+}
+
+
