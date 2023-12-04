b681a4c14e10a7de39af27e80e3bd57e133eeb24
HLaiK <lktul@unca.edu>
2023-12-04 16:06:43 -0500

    Adding my homepage

---
+++ quizzes/quiz03/main.js
@@ -0,0 +1,31 @@
+function color1() {
+    // target the element with the id of square1
+    // and change its background color...
+    document.querySelector("#square1").style.backgroundColor = "#205072";
+}
+
+function color2() {
+    // target the element with the id of square2
+    // and change its background color...
+    document.querySelector("#square2").style.backgroundColor = "#329D9C";
+}
+
+function color3() {
+    // TODO
+    document.querySelector("#square3").style.backgroundColor = "#56C596";
+}
+
+function color4() {
+    // TODO
+    document.querySelector("#square4").style.backgroundColor = "#7BE495";
+}
+
+function color5() {
+    // TODO
+    document.querySelector("#square5").style.backgroundColor = "#C5E8B7";
+}
+
+function color6() {
+    // TODO
+    document.querySelector("#square6").style.backgroundColor = "#CFF4D2";
+}
